c0c6a5ce774b36bb79d1fa721ff40bfe6c29556b
ali <ali@MacBook-Pro-Ali.local>
2023-01-03 03:12:29 +0400

    mesaj

---
+++ alqoritm suali 9/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace alqoritm_suali_9;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int max = 0;
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        for (int i = 0; i < numbers.Length; i++)
+
+        {
+            if (numbers[i] > numbers[max])
+                max = i;
+
+        }Console.WriteLine(max);
+
+
+    }
+}
+
